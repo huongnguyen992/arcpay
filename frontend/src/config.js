d9a329340e8f37504def06ae729240f8ab823308
linh <linhngoc99991@gmail.com>
2026-05-24 22:11:37 +0700

    revert chain ID to 5042002 per Arc docs

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,13 +1,13 @@
 // Thông tin Arc Testnet
 export const ARC_TESTNET = {
-  chainId: "0x4D0132",      // 5042002 dạng hex
+  chainId: "0x4CFF72",      // 5042002 dạng hex
   chainName:       "Arc Testnet",
   nativeCurrency:  { name: "USDC", symbol: "USDC", decimals: 18 },
   rpcUrls:         ["https://rpc.testnet.arc.network"],
   blockExplorerUrls: ["https://testnet.arcscan.app"],
 };
 
-export const CHAIN_ID_DEC = 5046130;
+export const CHAIN_ID_DEC = 5042002;
 // USDC ERC-20 trên Arc Testnet (6 decimals)
 export const USDC_ADDRESS = "0x3600000000000000000000000000000000000000";
 
